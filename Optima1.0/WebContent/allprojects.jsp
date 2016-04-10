d38fa727587e9b887360cbade799f4cbe04bd857
mohamed badr <mohamed_badr@yahoo.com>
2016-04-10 09:27:17 -0400

    delete button

--- Optima1.0/WebContent/allprojects.jsp
+++ Optima1.0/WebContent/allprojects.jsp
@@ -1,12 +1,19 @@
 <script>
-	var currentPage = 8;
+	var currentPage = 9;
 </script>
 <%@ include file="header.html"%>
 <div class="mainContainer">
 	<div id="main" style="width: 100%;"
 		class="ui-tabs ui-widget ui-widget-content ui-corner-all">
 		<label>All Projects</label> 
-		<div id="toolbarContainer" style="width: 100%;height:40px">toolbar</div>
+		<div id="toolbarContainer" style="width: 100%;height:40px">
+		<input type="text" name="searchBox" id="searchBox" style="width: 200px;margin-top:5px" class="text ui-widget-content ui-corner-all" placeholder="filter projects" autocomplete="off"/>
+		<div style="float:right;height:100%;width:300px">
+			<button id="addProject" class="addPortfolioBtn " style="width:16px;height:16px"></button>
+			<button id="editProject" class="projbuttonEdit" style="width:16px;height:16px"></button>
+			<button id="deleteProject" class="projbuttonDelete" style="width:16px;height:16px"></button>
+		</div>
+		</div>
 		<div id="gridContainer" class="form-section" style="width: 100%;height:600px"></div>
 
 	</div>
--- Optima1.0/WebContent/js/allprojects.js
+++ Optima1.0/WebContent/js/allprojects.js
@@ -8,15 +8,15 @@ var prepareProjectData = function(search) {
 	var result = [];
 	if (allProjects && allProjects.data && allProjects.data.list) {
 		var list = allProjects.data.list;
-		for ( var i=0;i<list.length;i++) {
+		for (var i = 0; i < list.length; i++) {
 			var proj = list[i];
 			var name = proj.projectName;
 			var code = proj.projectCode;
 			if (name && code) {
 				var include = true;
 				if (search) {
-					if (!search.indexOf(name.toLowerCase())
-							&& !search.indexOf(code.toLowerCase())) {
+					if (name.toLowerCase().indexOf(search)==-1
+							&& code.toLowerCase().indexOf(search)==-1) {
 						include = false;
 					}
 				}
@@ -31,11 +31,12 @@ var prepareProjectData = function(search) {
 						projectName : proj.projectName,
 						projectCode : proj.projectCode,
 						projectId : proj.projectCode,
-						proposedStartDate:formattedDate
+						proposedStartDate : formattedDate,
+						proj : proj
 					};
 					if (proj.portfolio && proj.portfolio.portfolioName) {
 						item.portfolioName = proj.portfolio.portfolioName;
-					} 
+					}
 					result.push(item);
 				}
 			}
@@ -43,6 +44,12 @@ var prepareProjectData = function(search) {
 	}
 	return result;
 }
+
+var enableButtons = function(enable) {
+	$('#editProject').prop("disabled", !enable);
+	$('#deleteProject').prop("disabled", !enable);
+}
+enableButtons(false);
 
 /*
  * $('#addUserBtn').on('click', function() { $("#addUserDialog").dialog("open");
@@ -80,4 +87,76 @@ $(function() {
 		enableColumnReorder : true
 	});
 
+	pGrid.setSelectionModel(new Slick.RowSelectionModel());
+	pGrid.onClick.subscribe(function(e) {
+		var cell = pGrid.getCellFromEvent(e);
+		pGrid.setSelectedRows(cell.row);
+		e.stopPropagation();
+		enableButtons(true);
+	});
+
+	pGrid.onDblClick.subscribe(function (e, args){
+	    var cell = pGrid.getCellFromEvent(e)
+	    var row = cell.row;
+	    var item = pGrid.getDataItem(row);
+	    if (item && item.proj && item.proj.projectId) {
+	    	window.location.href = "projectDetails.jsp?projectId=" + item.proj.projectId;
+	    }
+	});
+	
+	$("#searchBox").keyup(function(event){
+	    if(event.keyCode == 13){
+	    	var search = $("#searchBox").val();
+	    	var newData = prepareProjectData(search);
+	    	pGrid.setData(newData);
+	    	pGrid.invalidate();
+	    }
+	});
+	
+	
+	$('#addProject').on('click', function() { 
+		var x = 0;
+	  });
+	$('#editProject').on('click', function() { 
+		var row = pGrid.getSelectedRows();
+		if (row) {
+		    var item = pGrid.getDataItem(row);
+		    if (item) {
+		    	///
+		    }
+		}
+	  });
+	$('#deleteProject').on('click', function() { 
+		var row = pGrid.getSelectedRows();
+		if (row) {
+		    var item = pGrid.getDataItem(row);
+		    if (item && item.proj && item.proj.projectId) {
+	        	var projectId = item.proj.projectId;
+				var buttons = {
+						Yes : function() {
+							$(this).dialog("close");
+				        	var call =  rpcClient.projectService.remove( projectId );
+				        	 if (call.result == 0) {
+			        			 location.reload();
+							} else {
+								showMessage("Delete Project", 'Error:'
+										+ result.message, 'error');
+							}
+						},
+						No : function() {
+							$(this).dialog("close");
+							return false;
+						}
+					}
+					showMessage('Delete Project',
+							'The selected project will be deleted permanently!',
+							'warning', buttons);
+		    }
+		}
+	  });
+	/*
+	addProject
+	editProject 
+	deleteProject
+*/
 })
