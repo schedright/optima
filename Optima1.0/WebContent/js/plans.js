e85508981140e5898727999525fb0919c6e0ebd3
mohamed badr <mohamed_badr@yahoo.com>
2016-04-14 22:38:09 -0400

    Fix Capital Plan

--- Optima1.0/WebContent/js/plans.js
+++ Optima1.0/WebContent/js/plans.js
@@ -4,211 +4,228 @@ var plansList = planRet.data.map;
 var errorMessage = planRet.data.map.errors;
 
 $(function() {
-	var projects = plansList.data.list;
-	if (errorMessage) {
-		setTimeout(function() {
-			showMessage("Project Error",'<p>Error getting project details for</p>' + errorMessage,'error');
-		},0);
-	}
-	var allYears = [];
-	/*
-	 * for (var i=0;i<projects.length;i++) { var proj = projects[i].map; var
-	 * yearsMap = proj.Details.map; for (var k in yearsMap) { if (yearsMap[k]) {
-	 * if (allYears.indexOf(k)==-1) { allYears.push(k); } } } }
-	 */
+  var projects = plansList.data.list;
+  if (errorMessage) {
+    setTimeout(function() {
+      showMessage("Project Error", '<p>Error getting project details for</p>' + errorMessage, 'error');
+    }, 0);
+  }
+  var allYears = [];
+  /*
+   * for (var i=0;i<projects.length;i++) { var proj = projects[i].map; var yearsMap = proj.Details.map; for (var k in yearsMap) { if (yearsMap[k]) { if
+   * (allYears.indexOf(k)==-1) { allYears.push(k); } } } }
+   */
 
-	var dateParts = planDates.data.map.plan_start.split("/");
-	var yS = parseInt(dateParts[2]);
-	var dateParts2 = planDates.data.map.plan_end.split("/");
-	var yE = parseInt(dateParts2[2]);
+  var dateParts = planDates.data.map.plan_start.split("/");
+  var yS = parseInt(dateParts[2]);
+  var dateParts2 = planDates.data.map.plan_end.split("/");
+  var yE = parseInt(dateParts2[2]);
 
-	$('#refreshPlanBtn').prop('disabled', true);
-	
+  $('#refreshPlanBtn').prop('disabled', true);
 
-	allYears.sort();
-	var pColumns = [ {
-		id : "Year",
-		name : "Year",
-		field : "Year",
-		minWidth : 120
-	} ];
-	var month = ['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-	var sm = parseInt(dateParts[0]);
-	var em = parseInt(dateParts2[0]);
-	for (var i=yS;i<yE+1;i++) {
-		var E = 12;
-		if (i==yE) {
-			E=em;
-		}
-		for (var m=sm;m<E+1;m++) {
-			allYears.push(month[m] + ' ' + i);
-		}
-		sm = 1;
-	}
-	
-	for (var i = 0; i < allYears.length; i++) {
-		pColumns.push({
-			id : allYears[i],
-			name : allYears[i],
-			field : allYears[i],
-			minWidth : 120
-		});
-	}
-	pColumns.push({
-		id : "Total",
-		name : "Total",
-		field : "Total",
-		minWidth : 120
-	});
-	var pData = [];
-	var getTotal = function(map,d) {
-		var parts = d.split(' ');
-		var y = parts[1];
-		var m = parts[0];
-		if (map && map[y] && map[y].map && map[y].map[m]) {
-			return map[y].map[m];
-		} 
-		return 0;
-	};
-	var totalsRow = {};
-	var allTotal = 0;
-	for (var i = 0; i < projects.length; i++) {
-		var proj = projects[i].map;
-		if (!proj.Details) {
-			continue;
-		}
-		var yearsMap = proj.Details.map;
+  allYears.sort();
+  var pColumns = [
+    {
+      id : "Year",
+      name : "Year",
+      field : "Year",
+      minWidth : 120
+    }
+  ];
+  var month = [
+      '',
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+  ];
+  var sm = parseInt(dateParts[0]);
+  var em = parseInt(dateParts2[0]);
+  for (var i = yS; i < yE + 1; i++) {
+    var E = 12;
+    if (i == yE) {
+      E = em;
+    }
+    for (var m = sm; m < E + 1; m++) {
+      allYears.push(month[m] + ' ' + i);
+    }
+    sm = 1;
+  }
 
-		var row = {
-			"Year" : proj.Project.projectCode
-		};
-		var t = 0;
-		for (var p = 0; p < allYears.length; p++) {
-			var payment = getTotal(yearsMap,allYears[p]);
-			if (payment) {
-				t += payment;
-				row[allYears[p]] = parseFloat(payment).toFixed(2);
-				if (!totalsRow[allYears[p]]) {
-				  totalsRow[allYears[p]] = 0;
-				}
-				totalsRow[allYears[p]] += payment;
-			}
-		}
-		row["Total"] = parseFloat(t).toFixed(2);
-		allTotal += t;
-		pData.push(row); 	
-	}
-  for (var val in totalsRow) {
+  for (var i = 0; i < allYears.length; i++) {
+    pColumns.push({
+      id : allYears[i],
+      name : allYears[i],
+      field : allYears[i],
+      minWidth : 120
+    });
+  }
+  pColumns.push({
+    id : "Total",
+    name : "Total",
+    field : "Total",
+    minWidth : 120
+  });
+  var pData = [];
+  var getTotal = function(map,
+      d) {
+    var parts = d.split(' ');
+    var y = parts[1];
+    var m = parts[0];
+    if (map && map[y] && map[y].map && map[y].map[m]) {
+      return map[y].map[m];
+    }
+    return 0;
+  };
+  var totalsRow = {};
+  var allTotal = 0;
+  for (var i = 0; i < projects.length; i++) {
+    var proj = projects[i].map;
+    if (!proj.Details) {
+      continue;
+    }
+    var yearsMap = proj.Details.map;
+
+    var row = {
+      "Year" : proj.Project.projectCode
+    };
+    var t = 0;
+    for (var p = 0; p < allYears.length; p++) {
+      var payment = getTotal(yearsMap, allYears[p]);
+      if (payment) {
+        t += payment;
+        row[allYears[p]] = parseFloat(payment).toFixed(2);
+        if (!totalsRow[allYears[p]]) {
+          totalsRow[allYears[p]] = 0;
+        }
+        totalsRow[allYears[p]] += payment;
+      }
+    }
+    row["Total"] = parseFloat(t).toFixed(2);
+    allTotal += t;
+    pData.push(row);
+  }
+  for ( var val in totalsRow) {
     if (typeof totalsRow[val] === 'number') {
       totalsRow[val] = parseFloat(totalsRow[val]).toFixed(2)
     }
   }
 
-	totalsRow["Year"] = "Total";
-	totalsRow["Total"] = parseFloat(allTotal).toFixed(2);
-	pData.push(totalsRow);
+  totalsRow["Year"] = "Total";
+  totalsRow["Total"] = parseFloat(allTotal).toFixed(2);
+  pData.push(totalsRow);
 
-	var pGrid = new Slick.Grid("#projectPayments", pData, pColumns, {
-		editable : false,
-		enableAddRow : false,
-		enableCellNavigation : true,
-		enableColumnReorder : false
-	});
+  $('#planTabs').tabs({
+    activate : function(e,
+        ui) {
+      $.cookie('plan-selected-tab', ui.newTab.index(), {
+        path : '/'
+      });
+    },
+    active : $.cookie('plan-selected-tab')
+  });
 
-	$('input.datepicker').datepicker({
-		showOn : "button",
-		buttonImage : "images/calendar.png",
-		buttonImageOnly : true
-	});
+  var pGrid = new Slick.Grid(
+      "#projectPayments",
+      pData,
+      pColumns,
+      {
+        editable : true,
+        enableCellNavigation : true,
+        enableColumnReorder : true
+      });
 
-	$('#planTabs').tabs({
-		activate : function(e, ui) {
-			$.cookie('plan-selected-tab', ui.newTab.index(), {
-				path : '/'
-			});
-		},
-		active : $.cookie('plan-selected-tab')
-	});
-
-	if (planDates.result == 0) {
-		var data = planDates.data.map;
-		var sd = data.plan_start;
-		var ed = data.plan_end;
-
-		$("#pStartDateTxt").val(sd);
-		$("#pFinishDateTxt").val(ed);
-	}
-
-	$('#savePlanDatesBtn').on('click', function() {
-		var sd = $("#pStartDateTxt").val();
-		var ed = $("#pFinishDateTxt").val();
-		var ret = rpcClient.projectService.savePlanDates(sd, ed);
-		if (ret.result==0) {
-			location.reload();
-		}
-	});
-
-	var allProjectsCall = rpcClient.projectService.findAllLight();
-	var includedProject = rpcClient.projectService.findPlanProjectIds();
-	if (allProjectsCall.result == 0 && includedProject.result == 0) {
-
-		var inm = {};
-		var includedProjects = includedProject.data.list;
-		for (var i = 0; i < includedProjects.length; i++) {
-			var proj = includedProjects[i];
-			inm[proj.projectId] = true;
-		}
-
-		var allProjects = allProjectsCall.data.list;
-		for (var i = 0; i < allProjects.length; i++) {
-			var proj = allProjects[i];
-
-			var li = $('<li></li>').addClass('ui-state-default').attr('id',
-					proj.projectId).text(proj.projectCode);
-			li.attr('title', proj.projectCode);
-
-			if (inm[proj.projectId]) {
-				$("#includedProjects").append(li);
-			} else {
-				$("#allProjects").append(li);
-			}
-		}
-		
-	    $("#allProjects, #includedProjects").sortable({
-			connectWith : ".sortable",
-			item : '> li:not(.ui-state-disabled)',
-			dropOnEmpty : true,
-
-		    }).disableSelection();
-
-	    $("#allProjects").droppable(
-			    {
-				accept : "#includedProjects li",
-				hoverClass : "ui-state-hover",
-				drop : function(ev, ui) {
-				    var res = rpcClient.projectService.changePlanProject(ui.draggable.attr("id"),false);
-					$('#refreshPlanBtn').prop('disabled', false);
-				    return res.result==0;
-				}
-			    });
+  $('input.datepicker').datepicker({
+    showOn : "button",
+    buttonImage : "images/calendar.png",
+    buttonImageOnly : true
+  });
 
 
-	    $("#includedProjects").droppable(
-			    {
-				accept : "#allProjects li",
-				hoverClass : "ui-state-hover",
-				drop : function(ev, ui) {
-				    var res = rpcClient.projectService.changePlanProject(ui.draggable.attr("id"),true);
-					$('#refreshPlanBtn').prop('disabled', false);
-				    return res.result==0;
-				}
-			    });
+  if (planDates.result == 0) {
+    var data = planDates.data.map;
+    var sd = data.plan_start;
+    var ed = data.plan_end;
 
-	}
-	
-	$('#refreshPlanBtn').on('click', function() {
-		location.reload();
-	});
+    $("#pStartDateTxt").val(sd);
+    $("#pFinishDateTxt").val(ed);
+  }
 
+  $('#savePlanDatesBtn').on('click', function() {
+    var sd = $("#pStartDateTxt").val();
+    var ed = $("#pFinishDateTxt").val();
+    var ret = rpcClient.projectService.savePlanDates(sd, ed);
+    if (ret.result == 0) {
+      location.reload();
+    }
+  });
+
+  var allProjectsCall = rpcClient.projectService.findAllLight();
+  var includedProject = rpcClient.projectService.findPlanProjectIds();
+  if (allProjectsCall.result == 0 && includedProject.result == 0) {
+
+    var inm = {};
+    var includedProjects = includedProject.data.list;
+    for (var i = 0; i < includedProjects.length; i++) {
+      var proj = includedProjects[i];
+      inm[proj.projectId] = true;
+    }
+
+    var allProjects = allProjectsCall.data.list;
+    for (var i = 0; i < allProjects.length; i++) {
+      var proj = allProjects[i];
+
+      var li = $('<li></li>').addClass('ui-state-default').attr('id', proj.projectId).text(proj.projectCode);
+      li.attr('title', proj.projectCode);
+
+      if (inm[proj.projectId]) {
+        $("#includedProjects").append(li);
+      } else {
+        $("#allProjects").append(li);
+      }
+    }
+
+    $("#allProjects, #includedProjects").sortable({
+      connectWith : ".sortable",
+      item : '> li:not(.ui-state-disabled)',
+      dropOnEmpty : true,
+
+    }).disableSelection();
+
+    $("#allProjects").droppable({
+      accept : "#includedProjects li",
+      hoverClass : "ui-state-hover",
+      drop : function(ev,
+          ui) {
+        var res = rpcClient.projectService.changePlanProject(ui.draggable.attr("id"), false);
+        $('#refreshPlanBtn').prop('disabled', false);
+        return res.result == 0;
+      }
+    });
+
+    $("#includedProjects").droppable({
+      accept : "#allProjects li",
+      hoverClass : "ui-state-hover",
+      drop : function(ev,
+          ui) {
+        var res = rpcClient.projectService.changePlanProject(ui.draggable.attr("id"), true);
+        $('#refreshPlanBtn').prop('disabled', false);
+        return res.result == 0;
+      }
+    });
+
+  }
+
+  $('#refreshPlanBtn').on('click', function() {
+    location.reload();
+  });
 
 })
--- Optima1.0/WebContent/plans.jsp
+++ Optima1.0/WebContent/plans.jsp
@@ -1,66 +1,76 @@
 <script>
-	var currentPage = 7;
-	
+  var currentPage = 7;
 </script>
 <%@ include file="header.html"%>
 
 <div class="mainContainer">
+	<div id="main" style="width: 100%;"
+		class="main-body">
 	<div id="projectsGantt" class="gantt"></div>
-	<div id="planTabs" style="width:100%">
-	<ul>
-		<li><a href="#planDates">Finance Period</a></li>
-		<li><a href="#projectsList">Select Projects</a></li>
-		<li><a href="#projectPayments">Contractors' Payments</a></li>
-	</ul>
+	<div id="planTabs" style="width: 100%; height:50%">
+		<ul>
+			<li><a href="#planDates">Finance Period</a></li>
+			<li><a href="#projectsList">Select Projects</a></li>
+			<li><a href="#projectPaymentsTab">Contractors' Payments</a></li>
+		</ul>
 
-	<div id="planDates">
-		<div class="two-columns">
-
-		<label for="pStartDateTxt">Plan Start Date</label> <input type="text"
-			id="pStartDateTxt" class="datepicker" name="pStartDateTxt" />
-
-	</div>
-
-	<div class="two-columns right">
-
-		<label for="pFinishDateTxt">Plan Finish Date</label> 
-		<input type="text" id="pFinishDateTxt" name="pFinishDateTxt" class="datepicker" />
-		<div style="padding-top:15px">
-			<button id="savePlanDatesBtn">Save Finance Period</button>
-		</div>
-
-	</div>
-		
-	</div>
-	<div id="projectsList">
-		<div id="divTasksDepends" class="form-section">
+		<div id="planDates">
 			<div class="two-columns">
-				<label>All Projects</label>
-				<ul id="allProjects" class="sortable">
-				
-				</ul>
+
+				<label for="pStartDateTxt">Plan Start Date</label> <input
+					type="text" id="pStartDateTxt" class="datepicker"
+					name="pStartDateTxt" />
+
 			</div>
-			<div id="listConnect"></div>
-			<div class="two-columns">
-				<label>Included Projects</label>
-				<ul id="includedProjects" class="sortable">
-				</ul>
-				<button id="refreshPlanBtn" style="margin-top:10px">Refresh Plan</button>
+
+			<div class="two-columns right">
+
+				<label for="pFinishDateTxt">Plan Finish Date</label> <input
+					type="text" id="pFinishDateTxt" name="pFinishDateTxt"
+					class="datepicker" />
+				<div style="padding-top: 15px">
+					<button id="savePlanDatesBtn">Save Finance Period</button>
+				</div>
+
 			</div>
-	
+
+		</div>
+		<div id="projectsList" >
+			<div id="divTasksDepends" class="form-section">
+				<div class="two-columns">
+					<label>All Projects</label>
+					<ul id="allProjects" class="sortable">
+
+					</ul>
+				</div>
+				<div id="listConnect"></div>
+				<div class="two-columns">
+					<label>Included Projects</label>
+					<ul id="includedProjects" class="sortable">
+					</ul>
+					<button id="refreshPlanBtn" style="margin-top: 10px">Refresh
+						Plan</button>
+				</div>
+
+			</div>
+		</div>
+		<div id="projectPaymentsTab" >
+			<div id="projectPayments" class="projectPayment"></div>
 		</div>
 	</div>
-	<div id="projectPayments" style="width:95%;height:400px;"></div>
 	</div>
 </div>
 
 <script src="js/formutils.js">
+  
 </script>
 
 <script src="js/plans.js">
+  
 </script>
 
 <script src="js/projgantt.js">
+  
 </script>
 
 <%@ include file="footer.html"%>
