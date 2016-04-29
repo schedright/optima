9629fd9b46e3a7f28d4c381a2b664d6018e12463
mohamed badr <mohamed_badr@yahoo.com>
2016-04-29 18:59:29 -0400

    finance ui changes

--- Optima1.0/WebContent/finData.jsp
+++ Optima1.0/WebContent/finData.jsp
@@ -1,89 +1,83 @@
-<script>var currentPage=2;</script>
+<script>
+  var currentPage = 2;
+</script>
 <%@ include file="header.html"%>
 <div class="mainContainer">
- <ul id="paymentMenu" class="contextMenu" style="display:none;position:absolute">
-  	<li data="Delete">Delete</li>
-  </ul>
-  
-  <ul id="financeMenu" class="contextMenu" style="display:none;position:absolute">
-  	<li data="Delete">Delete</li>
-  </ul>
-  
-    <ul id="extraPaymentMenu" class="contextMenu" style="display:none;position:absolute">
-  	<li data="Delete">Delete</li>
-  </ul>
-  
-<div id="main" style="width:100%;padding:20px">
+	<div id="main" style="width: 100%;" class="main-body">
 
-<div id="FinPeriods" class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+		<ul id="paymentMenu" class="contextMenu"
+			style="display: none; position: absolute">
+			<li data="Delete">Delete</li>
+		</ul>
 
- 
-   <div id="finances" class="ui-tabs ui-widget ui-widget-content ui-corner-all ui-center"  style="width:90%;">
-   
-   <div class="form-section">
+		<ul id="financeMenu" class="contextMenu"
+			style="display: none; position: absolute">
+			<li data="Delete">Delete</li>
+		</ul>
 
-					<div class="two-columns">
+		<div id="main" style="width: 100%; padding: 20px">
+
+			<div id="FinPeriods"
+				class="ui-tabs ui-widget ui-widget-content ui-corner-all">
+				<label>Constraints</label>
+
+
+				<div id="finances"
+					class="ui-tabs ui-widget ui-widget-content ui-corner-all ui-center"
+					style="width: 90%;">
+
+					<!--div class="form-section">
+						<div class="two-columns">
+							<label for="financeAmount">Finance Amount</label> <input
+								type="text" id="financeAmount" name="financeAmount" class="" />
+						</div>
+						<div class="two-columns right">
+							<label for="financeDate">Finance Date</label> <input type="text"
+								id="financeDate" name="financeDate" class="" />
+						</div>
+					</div-->
+					<div id="ulContainer" class="form-section">
+						<div class="two-columns">
+							<ul id="financeList" class="sortable" style="height: initial">
+
+							</ul>
+						</div>
+						<div class="two-columns">
+							<p>
+								<button id="addFinanceBtn">Add Finance</button>
+							</p>
+							<p>
+								<button id="deleteFinanceBtn">Delete Finance</button>
+							</p>
+						</div>
+
+					</div>
+
+				</div>
+
+
+
+
+			</div>
+
+			<div id="addFinanceDialog" title="Add Finance">
+				<p class="validateTips">All form fields are required.</p>
+
+				<form id="addUserForm" autocomplete="off">
+					<fieldset>
 						<label for="financeAmount">Finance Amount</label> <input
 							type="text" id="financeAmount" name="financeAmount" class="" />
-					</div>
-					<div class="two-columns right">
-						<label for="financeDate">Finance Date</label> <input type="text"
+						<label for="interestRate">Interest Rate</label> <input type="text"
+							id="interestRate" name="interestRate" class="" /> <label
+							for="financeDate">Finance Date</label> <input type="text"
 							id="financeDate" name="financeDate" class="" />
-					</div>
+					</fieldset>
+				</form>
+			</div>
+			
+			<script src="js/formutils.js"></script>
+			<script src="js/finData.js"></script>
+		</div>
 	</div>
-		<div class="form-section">
-	<div class="two-columns">
-			<button id="addFinance">Add Finance</button>
-	</div>
-	</div>
-	
-		<table>
-		<tr>	
-		 <td valign="top" width="50%">
-   		  <div id="financeGrid" style="width:1000px; height:80px;"></div>
-   		</td>
-   		</tr>
-   		</table>
-   		
-   		
-   		
-	   <div class="form-section">
-					<div class="two-columns">
-						<label for="extraPaymentAmount">Loan Amount</label> <input
-							type="text" id="extraPaymentAmount" name="extraPaymentAmount"
-							class="" />
-					</div>
-					<div class="two-columns right">
-						<label for="extraPaymentDate">Loan Date</label> <input
-							type="text" id="extraPaymentDate" name="extraPaymentDate"
-							class="" />
-					</div>
-				</div>
-	
-	<div class="form-section">
-	<div class="two-columns">
-			<button id="addExtraPayment">Add Loan</button>
-	</div>
-	</div>
-	
-		<table>
-		<tr>	
-		 <td valign="top" width="50%">
-   		  <div id="extraPaymentGrid" style="width:1000px; height:80px;"></div>
-   		</td>
-   		</tr>
-   		</table>
-   	</div>
-   	
-   
-
-
- </div>
- 
-	
-  <script src="js/formutils.js"> </script> 
- <script src="js/finData.js"> </script> 
- 
-</div>
 </div>
 <%@ include file="footer.html"%>
--- Optima1.0/WebContent/js/finData.js
+++ Optima1.0/WebContent/js/finData.js
@@ -1,454 +1,300 @@
-
 $(function() {
-	document.title = 'SchedRight - Financial Periods';
+  document.title = 'SchedRight - Financial Periods';
 
-    $('#financeDate').datepicker({
-	showOn : "button",
-	buttonImage : "images/calendar.png",
-	buttonImageOnly : true
-    });
-    
-    $('#extraPaymentDate').datepicker({
-    	showOn : "button",
-    	buttonImage : "images/calendar.png",
-    	buttonImageOnly : true
-        });
+  var portfolioId = 0;
+  var projectId = 0;
 
-    var fColumns = [];
-    var eColumns = [];
-
-    
-    
-    $("#addFinance").button({
-	icons : {
-	    primary : "ui-icon-circle-plus"
-	},
-	text : true
-    }).click(function() {
-
-	var bValid = true;
-	var dateExists = false;
-	$("#financeAmount").removeClass("ui-state-error");
-	$("#financeDate").removeClass("ui-state-error");
-
-	var financeDate = $("#financeDate").val();
-	if (financeDate != null && financeDate.length != 0) {
-	    financeDate = new Date(financeDate);
-	    if (dateExistes(financeDate, fColumns)) {
-		$("#financeDate").addClass("ui-state-error");
-		dateExists = true;
-	    }
-	} else {
-	    $("#financeDate").addClass("ui-state-error");
-	    bvalid = false;
-	}
-	var financeAmount = $("#financeAmount").val();
-	if (financeAmount == null || financeAmount.length == 0 || !/^-?\d*\.?\d*$/.test(financeAmount)) {
-	    $("#financeAmount").addClass("ui-state-error");
-	    bValid = false;
-	}
-	if (dateExists) {
-    	showMessage("Add Finance","The entered date exists already in the finaces grid",'error');
-	    return;
-	}
-
-	if (bValid) {
-	    var createFinanceResult = rpcClient.financeService.create(portfolioId, financeAmount, financeDate);
-
-	    if (createFinanceResult.result == 0) {
-		location.reload(true);
-	    } else {
-	    	showMessage("Create Payment",'Error:' + createTaskResult.message,'error');
-	    }
-	} else {
-    	showMessage("Create Payment","Error: Please check your input",'error');
-	}
-    });
-    
-    $("#addExtraPayment").button({
-    	icons : {
-    	    primary : "ui-icon-circle-plus"
-    	},
-    	text : true
-        }).click(function() {
-
-    	var bValid = true;
-    	var dateExists = false;
-    	$("#extraPaymentAmount").removeClass("ui-state-error");
-    	$("#extraPaymentDate").removeClass("ui-state-error");
-
-    	var extraPaymentDate = $("#extraPaymentDate").val();
-    	if (extraPaymentDate != null && extraPaymentDate.length != 0) {
-    		extraPaymentDate = new Date(extraPaymentDate);
-    	    if (dateExistes(extraPaymentDate, eColumns)) {
-    		$("#extraPaymentDate").addClass("ui-state-error");
-    		dateExists = true;
-    	    }
-    	} else {
-    	    $("#extraPaymentDate").addClass("ui-state-error");
-    	    bvalid = false;
-    	}
-    	var extraPaymentAmount = $("#extraPaymentAmount").val();
-    	if (extraPaymentAmount == null || extraPaymentAmount.length == 0 || !/^-?\d*\.?\d*$/.test(extraPaymentAmount)) {
-    	    $("#extraPaymentAmount").addClass("ui-state-error");
-    	    bValid = false;
-    	}
-    	if (dateExists) {
-	    	showMessage("Add Payment","The entered date exists already in the Extra Payment grid",'error');
-    	    return;
-    	}
-
-    	if (bValid) {
-    	    var createExtraPaymentResult = rpcClient.extraPaymentService.create(portfolioId, extraPaymentAmount, extraPaymentDate);
-    	    if (createExtraPaymentResult.result == 0) {
-    		location.reload(true);
-    	    } else {
-		    	showMessage("Create Payment",'Error:' + createExtraPaymentResult.message,'error');
-    	    }
-    	} else {
-	    	showMessage("Create Payment","Error: Please check your input",'error');
-    	}
-        });
-    
-    
-    
-    
- /*   $("#FinPeriods").accordion({
-
-	activate : function(event, ui) {
-	    $.cookie('saved_index_fp', null);
-	    $.cookie('saved_index_fp', $("#FinPeriods").accordion("option", "active"));
-	},
-	active : parseInt($.cookie('saved_index_fp')),
-	heightStyle : "content"
-    });*/
-
-    var portfolioId = null;
-
-    for ( var i in getURLVariables()) {
-	if (i == "portfolioId") {
-	    portfolioId = getURLVariables()[i];
-	}
+  for ( var i in getURLVariables()) {
+    if (i == "portfolioId") {
+      portfolioId = getURLVariables()[i];
+    } else if (i == "projectId") {
+      projectId = getURLVariables()[i];
     }
+  }
+  var enableButtons = function(enable) {
+    $('#deleteFinanceBtn').prop("disabled", !enable);
+  }
+  $("#financeList").selectable();
+  var selected = {};
 
-    var result = rpcClient.projectService.findAllByPortfolio(portfolioId);
-    if (result.result == 0) {
-	var fmt = new DateFmt("%w %d-%n-%y");
-	var pColumns = [ {
-	    id : "project",
-	    name : "Project Code",
-	    field : "project",
-	    minWidth : 120,
-	    tag : "project",
-	    formatter : formatter
-	}, {
-	    id : "projectName",
-	    name : "Project Name",
-	    field : "projectName",
-	    tag : "projectName",
-	    minWidth : 180
-	},
+  $('#financeList').selectable({
+    selected : function(event,
+        ui) {
+      selected.selection = ui.selected;
+      enableButtons(true);
 
-	];
-	var pData = [];
-
-	var projectsList = result.data.list;
-
-	for (var i = 0; i < projectsList.length; i++) {
-
-	    pData[i] = {
-		project : "<a id= '" + projectsList[i].projectId + "' href='projectDetails.jsp?projectId="
-			+ projectsList[i].projectId + "' target='_blank' tabindex='0'>" + projectsList[i].projectCode
-			+ "</a>",
-		projectName : projectsList[i].projectName,
-
-	    }
-	    var projectPayments = projectsList[i].projectPayments.list;
-
-	    for (var j = 0; j < projectPayments.length; j++) {
-
-			var theDate = new Date(projectPayments[j].paymentDate.time);
-			var formattedDate = fmt.format(theDate);
-	
-			if (!isColumnExist(pColumns, formattedDate)) {
-				insertColumn(pColumns , formattedDate , theDate , Slick.Formatters.Currency);
-			}
-		
-		    if (pData[i][formattedDate] == null) {
-		    	pData[i][formattedDate] = 0;
-		    }
-		    
-		    var paymentAmount = projectPayments[j].paymentAmount;
-		    
-		    if (paymentAmount == 0 && projectPayments[j].paymentType.paymentType == "Intrim") {
-		    	paymentAmount = projectPayments[j].paymentInitialAmount;
-		    }
-		    pData[i][formattedDate] += paymentAmount;
-	    }
-	}
-
+      /*
+       * selected.selection = ui.selected; var user = allUsersMap[ui.selected.textContent]; $("#editUserBtn").html( user.isAdmin ? 'Remove Admin' : 'Make
+       * Admin'); enableButtons(true);
+       */
+    },
+    unselected : function(event,
+        ui) {
+      enableButtons(false);
+      selected.selection = null;
     }
+  });
 
-    var result = rpcClient.financeService.findAllByPortfolio(portfolioId);
-    if (result.result == 0) {
-	var fmt = new DateFmt("%w %d-%n-%y");
-	var fData = [];
-	fData[0] = {}
-	fColumns.length = 0;
-	var financesList = result.data.list;
+  $('#financeDate').datepicker({
+    showOn : "button",
+    buttonImage : "images/calendar.png",
+    buttonImageOnly : true
+  });
 
-	for (var i = 0; i < financesList.length; i++) {
-	    var theDate = new Date(financesList[i].financeUntillDate.time);
-	    fColumns.push({
-		id : financesList[i].financeId,
-		name : fmt.format(theDate),
-		tag : theDate,
-		field : "finance_" + financesList[i].financeId,
-		minWidth : 120 , formatter : Slick.Formatters.Currency
-	    });
-	    fData[0]["finance_" + financesList[i].financeId] =  financesList[i].financeAmount;
-	}
+  $("#addFinanceBtn").button({
+    text : true
+  }).click(function() {
+    $("#addFinanceDialog").dialog("open");
+  });
 
-	fColumns.sort(function(a, b) {
-	    return a.tag - b.tag;
-	});
-
-	var fGrid = new Slick.Grid("#financeGrid", fData, fColumns, {
-	    editable : true,
-	    enableAddRow : true,
-
-	    enableCellNavigation : true,
-	    enableColumnReorder : true
-	});
-
-	fGrid.onContextMenu.subscribe(function(e, args) {
-	    e.preventDefault();
-	    var cell = fGrid.getCellFromEvent(e);
-	    $("#financeMenu").data("cell", cell).css("top", e.pageY).css("left", e.pageX).show();
-
-	    $("body").one("click", function() {
-		$("#financeMenu").hide();
-	    });
-
-	});
-
-	$("#financeMenu").click(function(e) {
-	    if (!$(e.target).is("li")) {
-		return;
-	    }
-	    if (!fGrid.getEditorLock().commitCurrentEdit()) {
-		return;
-	    }
-	    var cell = $(this).data("cell");
-	    var financeId = fColumns[cell.cell].id;
-	    var deleteFinanceResult = rpcClient.financeService.remove(financeId);
-	    if (deleteFinanceResult.result == 0) {
-		fColumns.splice(cell.cell, 1);
-		fGrid.invalidate();
-		fGrid.render();
-		location.reload(true);
-	    } else {
-	    	showMessage("Delete Finance Result",'Error:' + deleteFinanceResult.message,'error');
-	    }
-	});
-
+  $("#deleteFinanceBtn").button({
+    text : true
+  }).click(function() {
+    var buttons = {
+      Yes : function() {
+        $(this).dialog("close");
+        var result = rpcClient.financeService.remove(selected.selection.id)
+        if (result && result.result == 0) {
+          // success, reload
+          location.reload();
+        } else {
+          showMessage("Delete Finance", 'Error:' + result.message, 'error');
+        }
+      },
+      No : function() {
+        $(this).dialog("close");
+        return false;
+      }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    var result = rpcClient.extraPaymentService.findAllByPortfolio(portfolioId);
-    if (result.result == 0) {
-	var emt = new DateFmt("%w %d-%n-%y");
-	var eData = [];
-	eData[0] = {}
-	eColumns.length = 0;
-	var extraPaymentsList = result.data.list;
+    showMessage('Delete Finance', 'The selected finance will be deleted permanently!', 'warning', buttons);
 
-	for (var i = 0; i < extraPaymentsList.length; i++) {
-	    var theDate = new Date(extraPaymentsList[i].extraPayment_date.time);
-	    eColumns.push({
-		id : extraPaymentsList[i].extraPayment_id,
-		name : fmt.format(theDate),
-		tag : theDate,
-		field : "extraPayment_" + extraPaymentsList[i].extraPayment_id,
-		minWidth : 120 , formatter : Slick.Formatters.Currency
-	    });
-	    eData[0]["extraPayment_" + extraPaymentsList[i].extraPayment_id] =  extraPaymentsList[i].extraPayment_amount;
-	}
+  });
 
-	eColumns.sort(function(a, b) {
-	    return a.tag - b.tag;
-	});
+  var result = rpcClient.financeService.findAllByPortfolio(portfolioId);
+  if (result.result == 0) {
+    var fmt = new DateFmt(
+        "%w %d-%n-%y");
+    var financesList = result.data.list;
+    var dateFormatter = new DateFmt(
+        "%d/%m/%y");
 
-	var eGrid = new Slick.Grid("#extraPaymentGrid", eData, eColumns, {
-	    editable : true,
-	    enableAddRow : true,
+    for (var i = 0; i < financesList.length; i++) {
+      var theDate = new Date(
+          financesList[i].financeUntillDate.time);
 
-	    enableCellNavigation : true,
-	    enableColumnReorder : true
-	});
+      var startDate = new Date(
+          financesList[i].financeUntillDate.time);
+      formattedDate = dateFormatter.format(startDate);
+      var title = formattedDate + "  -  " + financesList[i].financeAmount + "$ @ " + "%";
+      var li = $('<li></li>').addClass('ui-state-default').attr('id', financesList[i].financeId).text(title);
+      li.attr('title', title);
 
-	eGrid.onContextMenu.subscribe(function(e, args) {
-	    e.preventDefault();
-	    var cell = eGrid.getCellFromEvent(e);
-	    $("#extraPaymentMenu").data("cell", cell).css("top", e.pageY).css("left", e.pageX).show();
-
-	    $("body").one("click", function() {
-		$("#extraPaymentMenu").hide();
-	    });
-
-	});
-
-	$("#extraPaymentMenu").click(function(e) {
-	    if (!$(e.target).is("li")) {
-		return;
-	    }
-	    if (!eGrid.getEditorLock().commitCurrentEdit()) {
-		return;
-	    }
-	    var cell = $(this).data("cell");
-	    var extraPaymentId = eColumns[cell.cell].id;
-	    var deleteExtraPaymentResult = rpcClient.extraPaymentService.remove(extraPaymentId);
-	    if (deleteExtraPaymentResult.result == 0) {
-		eColumns.splice(cell.cell, 1);
-		eGrid.invalidate();
-		eGrid.render();
-		location.reload(true);
-	    } else {
-	    	showMessage("Delete Payment",'Error:' + deleteExtraPaymentResult.message,'error');
-	    }
-	});
-
+      $("#financeList").append(li);
     }
+  }
 
-});
-
-function getURLVariables() {
+  function getURLVariables() {
     var getVars = [];
     var split = location.href.split('?')[1].split('&');
     if (split != null) {
-	for ( var i in split) {
-	    var parts = split[i].split('=');
-	    getVars[parts[0]] = parts[1];
-	}
+      for ( var i in split) {
+        var parts = split[i].split('=');
+        getVars[parts[0]] = parts[1];
+      }
     }
 
     return getVars;
-}
+  }
 
-function DateFmt(fstr) {
+  function DateFmt(fstr) {
     this.formatString = fstr;
 
-    var mthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
-    var dayNames = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+    var mthNames = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ];
+    var dayNames = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"
+    ];
     var zeroPad = function(number) {
-	return ("0" + number).substr(-2, 2);
+      return ("0" + number).substr(-2, 2);
     };
 
     var dateMarkers = {
-	d : [ 'getDate', function(v) {
-	    return zeroPad(v);
-	} ],
-	m : [ 'getMonth', function(v) {
-	    return zeroPad(v + 1);
-	} ],
-	n : [ 'getMonth', function(v) {
-	    return mthNames[v];
-	} ],
-	w : [ 'getDay', function(v) {
-	    return dayNames[v];
-	} ],
-	y : [ 'getFullYear' ],
-	H : [ 'getHours', function(v) {
-	    return zeroPad(v);
-	} ],
-	M : [ 'getMinutes', function(v) {
-	    return zeroPad(v);
-	} ],
-	S : [ 'getSeconds', function(v) {
-	    return zeroPad(v);
-	} ],
-	i : [ 'toISOString' ]
+      d : [
+          'getDate',
+          function(v) {
+            return zeroPad(v);
+          }
+      ],
+      m : [
+          'getMonth',
+          function(v) {
+            return zeroPad(v + 1);
+          }
+      ],
+      n : [
+          'getMonth',
+          function(v) {
+            return mthNames[v];
+          }
+      ],
+      w : [
+          'getDay',
+          function(v) {
+            return dayNames[v];
+          }
+      ],
+      y : [
+        'getFullYear'
+      ],
+      H : [
+          'getHours',
+          function(v) {
+            return zeroPad(v);
+          }
+      ],
+      M : [
+          'getMinutes',
+          function(v) {
+            return zeroPad(v);
+          }
+      ],
+      S : [
+          'getSeconds',
+          function(v) {
+            return zeroPad(v);
+          }
+      ],
+      i : [
+        'toISOString'
+      ]
     };
 
     this.format = function(date) {
-	var dateTxt = this.formatString.replace(/%(.)/g, function(m, p) {
-	    var rv = date[(dateMarkers[p])[0]]();
+      var dateTxt = this.formatString.replace(/%(.)/g, function(m,
+          p) {
+        var rv = date[(dateMarkers[p])[0]]();
 
-	    if (dateMarkers[p][1] != null)
-		rv = dateMarkers[p][1](rv);
+        if (dateMarkers[p][1] != null)
+          rv = dateMarkers[p][1](rv);
 
-	    return rv;
+        return rv;
 
-	});
+      });
 
-	return dateTxt;
+      return dateTxt;
     };
 
-}
+  }
 
-function formatter(row, cell, value, columnDef, dataContext) {
+  function formatter(row,
+      cell,
+      value,
+      columnDef,
+      dataContext) {
     return value;
-}
+  }
 
-function isColumnExist(columns, elementId) {
-    for (var i = 0; i < columns.length; i++) {
-	if (columns[i]["id"] == elementId) {
-	    return true;
-	}
-    }
+  function dateExistes(date,
+      array) {
     return false;
-}
+    /*
+     * for (var i = 0; i < array.length; i++) { // alert('date1 - date2: ' + date - array[i].tag); if (array[i].tag - date === 0) return true; } return false;
+     */
+  }
 
-function sortDatesArray(array) {
-    alert('sort func ');
-    return array.sort(function(a, b) {
-	var c = new Date(a.tag);
-	alert('c: ' + c);
-	var d = new Date(b.tag);
-	alert('d: ' + d);
-	return c - d;
-    });
+  $("#addFinanceDialog").dialog({
+    autoOpen : false,
+    height : 350,
+    width : 300,
+    modal : true,
+    show : {
+      effect : "blind",
+      duration : 300
+    },
+    hide : {
+      effect : "fade",
+      duration : 300
+    },
+    buttons : {
+      "Add Finance" : function() {
+        var bValid = true;
+        var dateExists = false;
+        $("#financeAmount").removeClass("ui-state-error");
+        $("#interestRate").removeClass("ui-state-error");
+        $("#financeDate").removeClass("ui-state-error");
 
-}
+        var financeAmount = $("#financeAmount").val();
+        var interestRate = $("#interestRate").val();
+        var financeDate = $("#financeDate").val();
 
-function insertColumn(columns , formattedDate, date , formatter ) {
+        if (financeDate != null && financeDate.length != 0) {
+          financeDate = new Date(
+              financeDate);
+          if (dateExistes(financeDate)) {
+            $("#financeDate").addClass("ui-state-error");
+            dateExists = true;
+          }
+        } else {
+          $("#financeDate").addClass("ui-state-error");
+          bvalid = false;
+        }
+        if (financeAmount == null || financeAmount.length == 0 || !/^-?\d*\.?\d*$/.test(financeAmount)) {
+          $("#financeAmount").addClass("ui-state-error");
+          bValid = false;
+        }
+        if (interestRate == null || interestRate.length == 0 || !/^-?\d*\.?\d*$/.test(interestRate)) {
+          $("#interestRate").addClass("ui-state-error");
+          bValid = false;
+        } else {
+          var num = parseFloat(interestRate);
+          if (num<0 || num>100) {
+            $("#interestRate").addClass("ui-state-error");
+            bValid = false;
+          }
+        }
+        
+        if (dateExists) {
+          showMessage("Add Finance", "The entered date exists already in the finaces grid", 'error');
+          return;
+        }
 
-		
-	var l = 2;
-	while (l < columns.length  && columns[l].tag < date  ) {
-		l++;	
-	} 
-	
-	
-	for (var k = columns.length ; k > l ; k --){
-		columns[k] = columns[k-1];
-	}
-	
-	columns[l] = {
-	id : formattedDate,
-	name : formattedDate,
-	field : formattedDate,
-	tag : date,
-	minWidth : 120,
-	formatter : formatter
-	};
-	
-}
-// pColumns = sortDatesArray(pColumns);
+        if (bValid) {
+          var createFinanceResult = rpcClient.financeService.create(portfolioId, financeAmount, financeDate);
 
-
-function dateExistes(date, array) {
-    
-    for (var i = 0; i < array.length; i++) {
-	//alert('date1 - date2: ' + date - array[i].tag);
-	if (array[i].tag - date === 0)
-	    return true;
+          if (createFinanceResult.result == 0) {
+            location.reload(true);
+          } else {
+            showMessage("Create Payment", 'Error:' + createTaskResult.message, 'error');
+          }
+        } else {
+          showMessage("Create Payment", "Error: Please check your input", 'error');
+        }
+      },
+      Cancel : function() {
+        $(this).dialog("close");
+      }
+    },
+    close : function() {
     }
-    return false;
-}
+  });
+
+  enableButtons(false);
+});
